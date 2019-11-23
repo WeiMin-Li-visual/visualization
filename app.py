77e5794b3857ad3e44f166979cf3849ef07ae146
wangzhao <a1914411803@163.com>
2019-11-23 09:39:25 +0800

    LT model

--- app.py
+++ app.py
@@ -131,11 +131,131 @@ def basic_ic_1():
 def basic_ic_10():
     return ''
 
-
+app.debug=True
 # 选择单个影响力最大的种子基于lt模型（每个节点模拟一次）
-@app.route('/basicLt1')#王钊
+@app.route('/basicLt1')  # 王钊
 def basic_lt_1():
-    return ''
+    # file = open('static/data/test.txt', 'r')
+    # graph_data = file.read()
+    # file.close()
+    import random
+    import json
+    import copy
+    # 读取数据
+    networkTemp = []
+    with open('static/data/Wiki.txt', 'r') as networkFile:
+        # 设置节点数
+        number_of_nodes = 105
+
+        for line in networkFile.readlines():
+            linePiece = line.split()
+            networkTemp.append([int(linePiece[0]), int(linePiece[1])])
+    # 初始化权重矩阵
+    networkWeight = []
+    # 设置第一，第二中权值的剩余权值，防止入度的总和超过1
+    node_degree_1 = [1 for i in range(number_of_nodes)]
+    node_degree_2 = [1 for i in range(number_of_nodes)]
+    for i in range(3):
+        networkWeight.append([])
+        for j in range(number_of_nodes):
+            networkWeight[i].append([])
+            for k in range(number_of_nodes):
+                networkWeight[i][j].append(0)
+    # 设置权重
+    # 边的权重有三种设置方式，一种是从[0.1, 0.01, 0.001]中随机选一个，一种是都固定0.1，一种是节点的入度分之一
+    probability_list = [0.1, 0.01, 0.001]
+    degree = [0 for i in range(number_of_nodes)]  # 保存每个节点的度
+
+    for linePiece in networkTemp:
+        degree[linePiece[1] - 1] += 1
+        if node_degree_1[linePiece[1] - 1] >= 0.1:
+            k = random.choice(probability_list)
+            networkWeight[0][linePiece[0] - 1][linePiece[1] - 1] = k
+            node_degree_1[linePiece[1] - 1] -= k
+        elif 0.001 <= node_degree_1[linePiece[1] - 1] < 0.1:  # 去除剩余值过小的情况
+            networkWeight[0][linePiece[0] - 1][linePiece[1] - 1] = node_degree_1[linePiece[1] - 1]
+        if node_degree_2[linePiece[1] - 1] >= 0.1:
+            networkWeight[1][linePiece[0] - 1][linePiece[1] - 1] = 0.1
+            node_degree_2[linePiece[1] - 1] -= 0.1
+    for node in range(number_of_nodes):
+        for iteration in range(number_of_nodes):
+            if iteration != node:
+                if networkWeight[1][iteration][node]:
+                    networkWeight[2][iteration][node] = 1 / degree[node]
+
+    # 设置传给前端的节点数据边数据的json串
+    graph_data_json = {}
+    nodes_data_json = []
+    for node in range(number_of_nodes):
+        nodes_data_json.append({})
+        nodes_data_json[node]['attributes'] = {}
+        nodes_data_json[node]['attributes']['modularity_class'] = 0
+        nodes_data_json[node]['id'] = str(node)
+        nodes_data_json[node]['category'] = 0
+        nodes_data_json[node]['itemStyle'] = ''
+        nodes_data_json[node]['label'] = {}
+        nodes_data_json[node]['label']['normal'] = {}
+        nodes_data_json[node]['label']['normal']['show'] = 'false'
+        nodes_data_json[node]['name'] = str(node)
+        nodes_data_json[node]['symbolSize'] = 35
+        nodes_data_json[node]['value'] = 15
+        nodes_data_json[node]['x'] = 0
+        nodes_data_json[node]['y'] = 0
+    links_data_json = []
+    for link in networkTemp:
+        links_data_json.append({})
+        links_data_json[len(links_data_json) - 1]['id'] = str(len(links_data_json) - 1)
+        links_data_json[len(links_data_json) - 1]['lineStyle'] = {}
+        links_data_json[len(links_data_json) - 1]['lineStyle']['normal'] = {}
+        links_data_json[len(links_data_json) - 1]['name'] = 'null'
+        links_data_json[len(links_data_json) - 1]['source'] = str(link[0] - 1)
+        links_data_json[len(links_data_json) - 1]['target'] = str(link[1] - 1)
+    graph_data_json['nodes'] = nodes_data_json
+    graph_data_json['links'] = links_data_json
+    graph_data = json.dumps(graph_data_json)
+
+    def set_influence_LT(node_set, m):
+        """
+                基于LT模型计算node_set集合的影响力
+                :param node_set: 节点集合
+                :param m: 设置使用的权重矩阵
+                :return: 返回被激活的节点集合
+                """
+        active_nodes = node_set  # 存放被激活的节点，初始为node_set
+        last_influence = 0  # 存放最新的节点影响力，如果值为0就说明影响力传播结束
+        start = last_influence
+        while last_influence != len(active_nodes):
+            last_influence = len(active_nodes)  # 更新影响力
+            for new_active_node in active_nodes[start:last_influence]:
+                for nei_node in range(number_of_nodes):
+                    if networkWeight[method][new_active_node][nei_node] != 0 and nei_node not in active_nodes:
+                        # 将邻居节点的阈值减去边上的权重，如果阈值小于0，那么节点被激活
+                        theta[nei_node] -= networkWeight[method][new_active_node][nei_node]
+                        if theta[nei_node] <= 0:
+                            active_nodes.append(nei_node)
+            start = last_influence
+        return active_nodes
+
+    theta = []  # 保存每个节点的阈值
+    for iteration in range(number_of_nodes):  # 为每个节点随机设置阈值
+        theta.append(random.random())
+    # 基于LT模型，执行贪心算法找影响力最大的节点
+    active_records = []  # 用来存放每个节点的模拟结果也就是最后激活的节点们
+    max_node_influence = 0  # 用来存放比较过程中当前最大的影响力
+    method = 1  # 选择使用哪种权重进行
+    save_theta = copy.deepcopy(theta)  # 保存theta
+    for node in range(number_of_nodes):  # 遍历所有的节点，判断影响力
+        active_records.append([])
+        active_records[node] = set_influence_LT([node], method)  # 执行贪心算法，保存被激活的节点，第一个参数为列表
+        influence = len(active_records[node])
+        if influence > max_node_influence:
+            max_node_influence = influence
+            max_influence_node = node
+        theta = copy.deepcopy(save_theta)  # 恢复theta
+    active_records = json.dumps(active_records)
+    # 把你需要的数据给对应的页面
+    return render_template('basic_ic_1.html', graph_data=graph_data, active_records=active_records, max_node_influence=
+    max_node_influence, max_influence_node=max_influence_node)
 
 
 # 选择单个影响力最大的种子基于page rank
